246def78f45fa853d789686341920d1fc6d1e2b5
twilmo14atgmaildotcom <twilmo14@gmail.com>
2015-10-28 16:59:09 +0100

    windows attempt with udp
    tried to convert what we have in the udp file for windows

--- UDP/udpcli.cpp
+++ UDP/udpcli.cpp
@@ -8,40 +8,40 @@
 using namespace std;
 
 int main() {
-  int clientSocket, portNum, nBytes;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
+	int clientSocket, portNum, nBytes;
+	char buffer[1024];
+	struct sockaddr_in serverAddr;
+	socklen_t addr_size;
 
-  /*Create UDP socket*/
-  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	/*Create UDP socket*/
+	clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Configure settings in address struct*/
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7890);
-  serverAddr.sin_addr.s_addr = 16777343;
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	/*Configure settings in address struct*/
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(7890);
+	serverAddr.sin_addr.s_addr = 16777343;
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*Initialize size variable to be used later on*/
-  addr_size = sizeof serverAddr;
+	/*Initialize size variable to be used later on*/
+	addr_size = sizeof serverAddr;
 
-  while(1){
-    printf("Type a sentence to send to server:\n");
-    cin.getline( buffer, 1024);
-    //    fgets(buffer,1024,stdin);
-    printf("You typed: %s",buffer);
+	while (1) {
+		printf("Type a sentence to send to server:\n");
+		cin.getline(buffer, 1024);
+		//    fgets(buffer,1024,stdin);
+		printf("You typed: %s", buffer);
 
-    nBytes = strlen(buffer) + 1;
-    
-    /*Send message to server*/
-    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+		nBytes = strlen(buffer) + 1;
 
-    /*Receive message from server*/
-//                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+		/*Send message to server*/
+		sendto(clientSocket, buffer, nBytes, 0, (struct sockaddr *)&serverAddr, addr_size);
 
-//    printf("Received from server: %s\n",buffer);
+		/*Receive message from server*/
+	//                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
 
-  }
+	//    printf("Received from server: %s\n",buffer);
 
-  return 0;
+	}
+
+	return 0;
 }

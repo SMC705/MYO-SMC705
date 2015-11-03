c1b29485251ac9a15510a8aa026bd7ae7c48407c
paolomsn <paolo.mesiano@gmail.com>
2015-11-03 12:23:11 +0100

    keyboard2pd

---
+++ UDP/prova.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+  char buffer[4] = "cia";
+  cout << buffer << endl;
+
+  buffer = "bohl";
+  cout << buffer << endl;
+
+}

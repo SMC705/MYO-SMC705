4b1a5bb947c106bbb03840d555969dfcc7ca55c6
paolomsn <paolo.mesiano@gmail.com>
2015-11-25 22:51:55 +0100

    la madonna

---
+++ UDP/myo2pd/replicaTest/replicaTest/replicaTest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+// UDP libraries
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+
+const string userID = "01";
+const string fileName = "/Users/Paolo/Documents/AAU/2015-2016/Fall\ Semester\ Project/MYO-SMC705/Test\ Recordings/user" + userID + ".txt";
+
+
+int main() {
+
+    // input file
+    ifstream inputFile;
+    inputFile.open( fileName );
+    
+    while ( inputFile.eof() ) {
+        
+        string myString = inputFile.get();
+        cout << myString << endl;
+        
+    }
+    
+
+}

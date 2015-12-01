03e97d7a5d2537f2457b4c7171de42dda31515b8
paolomsn <paolo.mesiano@gmail.com>
2015-12-01 18:48:01 +0100

    update again

--- myo2pd/myo2pd/myo2pd.cpp
+++ myo2pd/myo2pd/myo2pd.cpp
@@ -23,8 +23,8 @@ using namespace std;
 // User ID
 //const string userID = "01";
 // Minimum and maximum roll angle
-const int minAngle = -102;
-const int maxAngle = -36;
+const int minAngle = -94;
+const int maxAngle = -12;
 // Filename to record user's actions
 //const string fileName = "/Users/Paolo/Documents/AAU/2015-2016/Fall\ Semester\ Project/MYO-SMC705/Test\ Recordings/user" + userID + ".txt";
 // Number of audio sources

be4e9805496388e71c5e693d267fa79b8f8d8a66
paolomsn <paolo.mesiano@gmail.com>
2015-12-10 21:35:47 +0100

    new test data
    Added user 19 & 20.
    Removed user 8 because of corrupted data.
    Shifted down usersID from 9 to 20.

--- myo2pd/myo2pd/myo2pd.cpp
+++ myo2pd/myo2pd/myo2pd.cpp
@@ -23,8 +23,8 @@ using namespace std;
 // User ID
 //const string userID = "01";
 // Minimum and maximum roll angle
-const int minAngle = 12;
-const int maxAngle = 104;
+const int minAngle = -10;
+const int maxAngle = 45;
 // Filename to record user's actions
 //const string fileName = "/Users/Paolo/Documents/AAU/2015-2016/Fall\ Semester\ Project/MYO-SMC705/Test\ Recordings/user" + userID + ".txt";
 // Number of audio sources
